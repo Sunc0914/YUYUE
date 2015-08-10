74f1557a4c66ba5e699cf2e81fb692bf2ce97619
MonkeyS914 <sunc@Sunc-3.local>
2015-08-10 18:35:20 +0800

    Initial Commit

---
+++ YUYUE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YUYUE
+//
+//  Created by Sunc on 15/8/10.
+//  Copyright (c) 2015年 Sunc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YUYUE/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YUYUE
+//
+//  Created by Sunc on 15/8/10.
+//  Copyright (c) 2015年 Sunc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
